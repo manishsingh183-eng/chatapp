c8e96c671c7a3872bb9dd01405e32b81bb96ed5a
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-06-11 14:16:00 +0530

    4th commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/chatApp/',
+  base: '/', 
   plugins: [react()],
   build: {
-    outDir: 'dist'  // <-- default for Vite and expected by Vercel
+    outDir: 'dist' // this is the default and correct for Vercel
   },
 })
-
