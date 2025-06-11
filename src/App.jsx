6869ba4ff06435b9d2a23af7e09392491417432a
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-06-11 16:29:49 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import {useAuthState} from "react-firebase-hooks/auth";
-import {createContext, useEffect} from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { createContext, useEffect } from "react";
 import "./App.css";
-import {SignIn} from "./components/Auth.jsx";
-import {auth} from "./config.js";
-import {Routes, Route, useNavigate} from "react-router-dom";
+import { SignIn } from "./components/Auth.jsx";
+import { auth } from "./config.js";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Room from "./components/Room.jsx";
 
 export const UserContext = createContext("")
@@ -13,22 +13,22 @@ function App() {
     const navigate = useNavigate();
 
     useEffect(() => {
-            if (!loading) {
-                if (user) {
-                    if (!window.location.pathname.startsWith("/chatApp/home"))
-                        navigate("/home");
-                } else {
-                    navigate("/sign-in")
-                }
+        if (!loading) {
+            if (user) {
+                if (!window.location.pathname.startsWith("/chatapp/home"))
+                    navigate("/home");
+            } else {
+                navigate("/sign-in")
             }
-        }, [user, loading]
+        }
+    }, [user, loading]
     )
     return (
         <div className="flex flex-col items-center">
             <UserContext.Provider value={user}>
                 <Routes>
-                    <Route path="/home/*" element={<Room/>}/>
-                    <Route path="/sign-in" element={<SignIn/>}/>
+                    <Route path="/home/*" element={<Room />} />
+                    <Route path="/sign-in" element={<SignIn />} />
                 </Routes>
             </UserContext.Provider>
         </div>
