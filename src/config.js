03340abd34e0545f8c68d2169295ee2690c26334
manishsingh183-eng <18manishsingh.3@gmail.com>
2025-06-11 16:05:16 +0530

    commit

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const firebaseApp = initializeApp({
   apiKey: "AIzaSyANt5f9BMdDFqgsNL7tYfctQJ4NVxfpudU",
   authDomain: "chatapp-43202.firebaseapp.com",
   projectId: "chatapp-43202",
-  storageBucket: "chatapp-43202.firebasestorage.app",
+  storageBucket: "chatapp-43202.appspot.com",
   messagingSenderId: "499362633040",
   appId: "1:499362633040:web:8bebec95683bb44343c855"
 });
